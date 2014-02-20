997e45ef3b1c5f2aefebf93707e6c1e9daebe5b7
Fajkowsky <dawidfajkowski@gmail.com>
2014-02-20 10:37:43 +0100

    added commented methods

--- ZBP_HashTable/HashTable.h
+++ ZBP_HashTable/HashTable.h
@@ -6,11 +6,16 @@ class HashTable
 public:
 	HashTable(void);
 	~HashTable(void);
+
+	//std::pair<iterator,bool> insert(const T& value);
+	//void erase( iterator pos );
+	//iterator find(const T& key);
 private:
 	struct field {
 		unsigned char state;
 		T value;
 	};
 	field hash_table[2];
+	//void resize();
 };
 
