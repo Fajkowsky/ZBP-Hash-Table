59bd69f2d097ce68aaac186c7c63b3ef754a5a4f
Aneta <aneta.harnasz@gmail.com>
2014-02-20 11:07:35 +0100

    exemple hashing function object

--- ZBP_HashTable/main.cpp
+++ ZBP_HashTable/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include "HashTable.h"
+#include <string>
 
 void main(){
+
+	struct myclass {
+		int operator() (int value, bool step, int table_size) { 
+			int index;
+			index = value % table_size;
+
+			if(step){
+				index = (3 + (value % 3));
+			}
+			return (index);
+		}
+	};
+
+	struct myStringclass {
+		int operator() (std::string value, bool step, int table_size) { 
+			int b,index = 0;
+			int a = 29;
+
+			if(!step){
+				for (unsigned int i = 0; i < value.length(); i++){
+					index = (a * index + value[i]) % table_size;
+				}
+			}
+			a = 31415;
+			b = 27183;
+			if(step){
+				for (unsigned int i = 0; i < value.length(); i++){
+					index = (a * index + value[i]) % table_size;
+					a = (a * b) % (table_size - 1);
+				}
+			}
+
+			return (index);
+		}
+	};
+
 	HashTable<int> tablica;
 	std::cout << "Test";
 	getchar();
